8da4f4aa206ef0d6320f5cbd5012b4dbcdc1f418
AhmedOmi <54010086+AhmedOmi@users.noreply.github.com>
2020-04-02 14:32:48 +0100

    Update console.py

--- console.py
+++ console.py
@@ -42,6 +42,7 @@ class HBNBCommand(cmd.Cmd):
             if not line:
                 raise SyntaxError()
             my_list = line.split(" ")
+            lib = {}
             for i in range(my_list):
                 if my_list[0] == "=" or my_list[-1] == "=" \
                                      or "=" not in my_list[i]:
