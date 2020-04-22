4d8fd344abb213c715ad2b8f4563b4f55c0625eb
ggirlk <ggirlk2@gmail.com>
2020-04-22 22:29:12 +0000

    python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Hello Flask """
+
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c():
+    return 'C %s' % escape(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python():
+    str = escape(text)
+    str.replace(' ', '_')
+    return 'Python %s' % escape(str)
+
+if __name__ == "__main__":
+    app.run()
