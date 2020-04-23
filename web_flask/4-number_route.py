6f4198e7a7b655dcf3833f4fc238df62e0d1c914
ggirlk <ggirlk2@gmail.com>
2020-04-23 02:19:58 +0000

    int route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,7 +28,7 @@ def python(text="is cool"):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def nbr(n):
     if isinstance(n, int):
         return "{} is a number".format(int(n))
