b397b2ddbf745b8febcdb95b3caa4db8b4f7ef3b
ggirlk <ggirlk2@gmail.com>
2020-04-23 01:23:07 +0000

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c(text):
 
 
 @app.route('/python/', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return 'Python {}'.format(text.replace('_', ' '))
 
