9bbaee68d2044970eba8670b06b3ee716a8578dc
ggirlk <ggirlk2@gmail.com>
2020-04-23 04:11:27 +0000

    template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Hello Flask """
+
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def nbr(n):
+    if isinstance(n, int):
+        return "{} is a number".format(int(n))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def tpl(n):
+    if isinstance(n, int):
+        return render_template("templates/5-number.html", n=n)
+
+if __name__ == "__main__":
+    app.run()
